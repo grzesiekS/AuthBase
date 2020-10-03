48430a05b1bad7661c67c855d2a638dd8effd905
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-03 16:36:17 +0200

    Prepare server to use OAuth

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
 
@@ -12,6 +15,29 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// configure passport provider options
+passport.use(new GoogleStrategy({
+  clientID: '1001460385010-l6sv2pur7vhcaigb7a0ugaao7ic8rkh3.apps.googleusercontent.com',
+  clientSecret: 'Ta1Ogd-aWfMnVUpv1HzlyNMO',
+  callbackURL: 'http://localhost:8000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  done(null, profile);
+}));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
 
 app.get('/', (req, res) => {
   res.render('index');
